546b1b0ccc82925f4ea6dd62a81bc56199a86044
Ayush Thakur <AT382377@GMAIL.COM>
2022-10-02 23:04:21 -0700

    Update App.java

--- NortonAssessment2/src/main/java/Exoplanet/NortonAssessment2/App.java
+++ NortonAssessment2/src/main/java/Exoplanet/NortonAssessment2/App.java
@@ -27,17 +27,17 @@ public class App
     public static void main( String[] args ) throws FileNotFoundException, IOException, ParseException{
     	RemoteDownloadFile object1 = new RemoteDownloadFile();
 		
-		
-//		try {
-//			URL url = new URL("https://gist.githubusercontent.com/joelbirchler/66cf8045fcbb6515557347c05d789b4a/raw/9a196385b44d4288431eef74896c0512bad3defe/exoplanets");
-//			HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
-//			if(connection.getResponseCode() == 200) {
-//				object1.remoteDownloadFile(connection);
-//			}
-//		}
-//		catch(Exception e) {
-//			System.out.println(e);
-//		}
+		//Code to setup connection with the JSON URL and then download it locally
+		try {
+			URL url = new URL("https://gist.githubusercontent.com/joelbirchler/66cf8045fcbb6515557347c05d789b4a/raw/9a196385b44d4288431eef74896c0512bad3defe/exoplanets");
+			HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
+			if(connection.getResponseCode() == 200) {
+				object1.remoteDownloadFile(connection);
+			}
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
 		
 		
 
